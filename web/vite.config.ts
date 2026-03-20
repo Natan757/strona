819d622513105e35e741a87695bbaad7921c9848
Natan757 <76836340+Natan757@users.noreply.github.com>
2026-03-20 21:55:36 +0100

    Fix filter syntax in Vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -62,7 +62,7 @@ export default defineConfig({
         plugins: ['styled-jsx/babel'],
       },
     }),
-      ]!filter(Boolean)
+      ].filter(Boolean)
     restart({
       restart: [
         'src/**/page.jsx',
